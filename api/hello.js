14a13f76bc1f3b9649eddc0d41bb42085b20f6e2
ivanmunozsolis <ivanmunozsolis37@gmail.com>
2025-12-27 00:51:25 -0500

    Update hello.js

--- api/hello.js
+++ api/hello.js
@@ -1,3 +1,7 @@
-export default function handler(req, res) {
-  res.status(200).json({ ok: true });
-}
+module.exports = (req, res) => {
+  res.status(200).json({
+    ok: true,
+    message: "Hola Iván, el backend funciona 🚀"
+  });
+};
+
